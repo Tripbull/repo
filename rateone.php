49af56a490ead788d904d7bb2b1956ac66ffd0c5
tripbull <robert.garlope@tabluu.com>
2015-03-23 09:53:06 +0800

    merge mac camera bug

--- rateone.php
+++ rateone.php
@@ -88,8 +88,8 @@ $connect->db_disconnect();
 				<div id="data">
 					<div class="cam-frame">
 					<div style="height:480px;">
-						<canvas id="canvas" style="position:absolute;" width="640" height="480"></canvas>
-						<video id="video" stlye="position:absolute;" width="640" height="480" autoplay></video>
+						<canvas id="canvas" style="position:absolute;width:100%" width="640" height="480"></canvas>
+						<video id="video" stlye="position:absolute;width:100%" width="640" height="480" autoplay></video>
 					</div>
 					<!-- <div id="screen"></div> -->
 					<div class="snapshotbutton">
